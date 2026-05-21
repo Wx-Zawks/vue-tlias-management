d679d2f76c29388e5fe675c95176bda351f0299c
wxz <1285623719@qq.com>
2026-05-21 17:42:55 +0800

    ``` 5.21完成文档17-前端Web实战(Tlias案例-员工管理) feat(emp): 添加员工新增功能的表单验证和数据重置
    - 在新增员工对话框中添加表单验证规则，包括用户名、姓名、性别、手机号的校验
    - 实现表单数据重置功能，在打开新增对话框时清空表单内容和校验提示
    - 添加表单验证逻辑和新增员工保存功能
    - 修改对话框底部按钮事件处理，点击保存执行表单验证和数据提交
    - 配置el-form的rules属性以启用表单验证功能
    ```

--- src/views/emp/index.vue
+++ src/views/emp/index.vue
@@ -124,8 +124,24 @@ const employee = ref({
 const addEmp = () => {
   dialogVisible.value = true
   dialogTitle.value = '新增员工' 
-}
 
+    //清空表单内容及校验提示信息
+  employee.value = {
+    username: '',
+    name: '',
+    gender: '',
+    phone: '',
+    job: '',
+    salary: '',
+    deptId: '',
+    entryDate: '',
+    image: '',
+    exprList: []
+  }
+  if(employeeFormRef.value){
+      employeeFormRef.value.resetFields()
+  }
+}
 //文件上传
 // 图片上传成功后触发
 const handleAvatarSuccess = (response,uploadFile) => {
@@ -163,6 +179,42 @@ watch(()=>employee.value.exprList, (newValue, oldValue) => {
     })
   }
 }, {deep: true});
+
+//表单校验规则
+// 验证规则
+const rules = ref({
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 2, max: 20, message: '用户名长度应在2到20个字符之间', trigger: 'blur' }
+  ],
+  name: [
+    { required: true, message: '请输入姓名', trigger: 'blur' },
+    { min: 2, max: 10, message: '姓名长度应在2到10个字符之间', trigger: 'blur' }
+  ],
+  gender: [
+    { required: true, message: '请选择性别', trigger: 'change' }
+  ],
+  phone: [
+    { required: true, message: '请输入手机号', trigger: 'blur' },
+    { pattern: /^1\d{10}$/g, message: '请输入有效的手机号', trigger: 'blur' }
+  ]
+});
+
+//保存新增员工
+const saveAddedEmp = async () => {
+  employeeFormRef.value.validate(async valid => {
+    if(valid){ // 校验通过
+      const result = await addEmpApi(employee.value);
+      if(result.code){
+        ElMessage.success('新增员工成功')
+        dialogVisible.value = false
+        search()
+      }else {
+        ElMessage.error(result.msg)
+      }
+    }
+  })
+}
 </script>
 
 <template>
@@ -251,19 +303,19 @@ watch(()=>employee.value.exprList, (newValue, oldValue) => {
   </el-pagination>
 
    <!-- 新增/修改员工的对话框 -->
-  <el-dialog v-model="dialogVisible" :title="dialogTitle">
-      <el-form ref="employeeFormRef" :model="employee" label-width="80px">
+  <el-dialog v-model="dialogVisible" :title="dialogTitle" :show-close="false">
+      <el-form ref="employeeFormRef" :model="employee" :rules="rules" label-width="80px">
         <!-- 基本信息 -->
         <!-- 第一行 -->
         <el-row :gutter="20">
           <el-col :span="12">
-            <el-form-item label="用户名">
+            <el-form-item label="用户名" prop="username">
               <el-input v-model="employee.username" placeholder="请输入员工用户名，2-20个字"></el-input>
             </el-form-item>
           </el-col>
           
           <el-col :span="12">
-            <el-form-item label="姓名">
+            <el-form-item label="姓名" prop="name">
               <el-input v-model="employee.name" placeholder="请输入员工姓名，2-10个字"></el-input>
             </el-form-item>
           </el-col>
@@ -282,7 +334,7 @@ watch(()=>employee.value.exprList, (newValue, oldValue) => {
           </el-col>
 
           <el-col :span="12">
-            <el-form-item label="手机号">
+            <el-form-item label="手机号" prop="phone">
               <el-input v-model="employee.phone" placeholder="请输入员工手机号"></el-input>
             </el-form-item>
           </el-col>
@@ -387,8 +439,8 @@ watch(()=>employee.value.exprList, (newValue, oldValue) => {
         <!-- 底部按钮 -->
         <template #footer>
           <span class="dialog-footer">
-            <el-button @click="dialogVisible = false">取消</el-button>
-            <el-button type="primary" @click="">保存</el-button>
+            <el-button @click="dialogVisible = false; employee = {}">取消</el-button>
+            <el-button type="primary" @click="saveAddedEmp">保存</el-button>
           </span>
         </template>
 
